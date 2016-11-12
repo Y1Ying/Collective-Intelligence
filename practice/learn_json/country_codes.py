f8ca6f74e8fd17b8efaf7da674084e8a957a1706
YinXin <yinxin0476@vip.qq.comm>
2016-11-12 19:08:34 +0800

    Json文件格式
    通过Pygal的maps.world，对人口信息在世界地图上显示，学习json数据格式，
    PS：Pygal的pygal.Worldmap()现在变更为pygal.maps.world.World()

---
+++ practice/learn_json/country_codes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from pygal.maps.world import COUNTRIES
+
+
+def get_country_code(country_name):
+    """根据指定的国家，返回pygal使用的两个字母的国别码"""
+    for code,name in COUNTRIES.items():
+        if name == country_name:
+            return code
+
+    # 如果没有找到指定的国家，就返回None
+    return None
+
+# print(get_country_code('Andorra'))
+# print(get_country_code('Afghanistan'))
