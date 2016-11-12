bc2d74143f340a171f81d9c50c2595846f6aa088
YinXin <yinxin0476@vip.qq.comm>
2016-11-12 15:06:12 +0800

    掷骰子
    使用Pygal模拟掷骰子，并用直方图显示结果

---
+++ practice/dice/die_visual.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import pygal
+
+from die import Die
+# 创建两个6面的骰子
+
+die_1 = Die()
+die_2 = Die()
+
+# 掷几次骰子，并将结果存储在一个列表中
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# 分析结果
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1,max_result+1):
+    frequency =results.count(value)
+    frequencies.append(frequency)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+hist.title = "Result of rolling two D6 1000 times"
+hist.x_labels = ['1','2','3','4','5','6','7','8','9','10','11','12']
+hist.x_title = 'Result'
+hist.y_title = 'Frequency of Result'
+
+hist.add('D6 + D6',frequencies)
+hist.render_to_file('die_visual.svg')
+
+print(frequencies)
